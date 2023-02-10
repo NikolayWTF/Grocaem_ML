e3852fcd305a4a6c833625a5a0547a07014e5715
Nikolay_WTF <kolan.gregorin01@gmail.com>
2023-02-10 13:26:28 +0300

    Нейронная сеть определяет, при какой комбинации сигналов светофора можно переходить дорогу. Обучение на полном наборе данных

--- Lesson4/traffic_light_problem.py
+++ Lesson4/traffic_light_problem.py
@@ -12,16 +12,20 @@ streetlights = np.array([[1, 0, 1],
 walk_vs_stop = np.array([0, 1, 0, 1, 1, 0])
 
 weights = np.array([1.0, 1.0, 1.0])
-alpha = 0.1 # При alpha = 1, алгоритм рассходится
+alpha = 0.1 # При alpha = 1, алгоритм рассходится, а при alpha = 0.01 - сходится но медленно
 
-inp = streetlights[0]
-goal_prediction = walk_vs_stop[0]
+for iteration in range(40):
+    error_for_all_lights = 0
+    for row_index in range(len(walk_vs_stop)):
+        inp = streetlights[row_index]
+        goal_prediction = walk_vs_stop[row_index]
 
-for iteration in range(20):
-    prediction = inp.dot(weights)
-    error = (goal_prediction - prediction) ** 2
-    delta = prediction - goal_prediction
-    weights_delta = inp * delta
-    weights -= alpha * weights_delta
+        prediction = inp.dot(weights)
 
-    print("Error: " + str(error) + " Prediction: " + str(prediction))
+        error = (prediction - goal_prediction) ** 2
+        delta = prediction - goal_prediction
+        weights_delta = inp * delta
+        weights -= alpha * weights_delta
+
+        print("Prediction: " + str(prediction))
+    print("Error: " + str(error) + "\n")
